ed3f77ef847fd048ccf21b16a5066bec64bee458
khyahahati <132439126+khyahahati@users.noreply.github.com>
2023-08-19 22:44:19 +0530

    Create Add two numbers.c

---
+++ Add two numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,sum=0;
+    
+    printf("Enter two numbers A and B");
+    
+    scanf("%i%i", &A , &B);
+    
+    sum = A+B;
+    
+    printf("Sum is : %i" , sum);
+    return 0;
+}
+
+/*NOTES
+1. scanf() - reads input from user
+*/
